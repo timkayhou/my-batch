8203157f5e72829affd064905357af6a0a52da30
Expurgator <44568943+timkayhou@users.noreply.github.com>
2022-12-26 05:36:57 +0900

    Add files via upload

---
+++ sh/steam-deck-enable-zh.sh
@@ -0,0 +1,9 @@
+ #!/bin/sh
+sudo steamos-readonly disable
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman -S glibc --noconfirm --ask=4
+sudo sed -i s/#zh/zh/ /etc/locale.gen
+sudo locale-gen
+sudo locale -a | grep "zh_..." | sudo tee -a /etc/locale.conf
+sudo sed -i s/^zh/LANG=zh/ /etc/locale.conf
