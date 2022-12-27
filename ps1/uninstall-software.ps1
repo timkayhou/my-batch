a0b665901bab26d12b3b2a71e6788e3285d017e5
Expurgator <44568943+timkayhou@users.noreply.github.com>
2022-12-27 23:37:23 +0900

    Add files via upload

--- ps1/uninstall-software.ps1
+++ ps1/uninstall-software.ps1
@@ -3,7 +3,7 @@ Get-AppxPackage >> D:\AppxPackage.bak1.txt
 # 小娜
 Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
 # 获取帮助
-Remove-AppxPackage -allusers Microsoft.GetHelp_10.2108.42428.0_x64__8wekyb3d8bbwe
+Remove-AppxPackage -allusers Microsoft.GetHelp_10.2208.2551.0_x64__8wekyb3d8bbwe
 # 人脉
 Remove-AppxPackage -allusers Microsoft.People_10.2105.4.0_x64__8wekyb3d8bbwe
 # 手机连接
